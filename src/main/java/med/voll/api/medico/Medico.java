7b9d7891e3783511afbc12263098b7106c1094ef
Dgley ramos <dgleysilva@gmail.com>
2023-02-22 15:13:55 -0300

    migration: alterando tabela medicos e adicionando o telefone

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -25,6 +25,7 @@ public class Medico {
   public Medico(DadosCadastroMedico dados) {
     this.nome = dados.nome();
     this.email = dados.email();
+    this.telefone = dados.telefone();
     this.crm = dados.crm();
     this.especialidade = dados.especialidade();
     this.endereco = new Endereco(dados.endereco());
@@ -35,6 +36,7 @@ public class Medico {
   private Long id;
   private String nome;
   private String email;
+  private String telefone;
   private String crm;
 
   @Enumerated(EnumType.STRING)
